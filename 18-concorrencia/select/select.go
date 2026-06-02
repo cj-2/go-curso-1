81ddd26d5198eb95c8885a3cac7ba26d3eab640f
Carlos Júnior <carlos.roberto.98jr@gmail.com>
2026-06-02 11:20:30 -0300

    Add examples of buffered channels and select statement in Go

---
+++ 18-concorrencia/select/select.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal1, canal2 := make(chan string), make(chan string)
+
+	go func() {
+		for {
+			time.Sleep(time.Millisecond * 500)
+			canal1 <- "Canal 1"
+		}
+	}()
+
+	go func() {
+		for {
+			time.Sleep(time.Second * 2)
+			canal2 <- "Canal 2"
+		}
+	}()
+
+	// for {
+	// 	mensagamCanal1 := <-canal1
+	// 	fmt.Println(mensagamCanal1)
+
+	// 	mensagamCanal2 := <-canal2
+	// 	fmt.Println(mensagamCanal2)
+	// }
+
+	for {
+		select {
+		case mensagamCanal1 := <-canal1:
+			fmt.Println(mensagamCanal1)
+		case mensagamCanal2 := <-canal2:
+			fmt.Println(mensagamCanal2)
+		}
+	}
+
+}
